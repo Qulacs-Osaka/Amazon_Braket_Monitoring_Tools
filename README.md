58d20019d826861932fa74edc0e71213ba29b937
speed1313 <speedtry13@icloud.com>
2022-03-30 12:32:30 +0900

    remove me_time and fix README

--- README.md
+++ README.md
@@ -3,8 +3,28 @@ Tools to monitor Amazon Braket
 
 このリポジトリはAmazon Braketを利用する上でのタスク監視、削除の支援ツールのコード置き場です。
 
+## AmazonBraketlib class
+Braket taskを監視・削除する基本メソッドが含まれたクラス.
 
-### shot.py 
+### AmazonBraketlibの主なメソッド
+- get_info(year, month, day, device_type, device_provider, device_name, index_of_status_type)
+指定した日付の指定したデバイスのタスクに関する情報をjson形式で出力する.
+出力されるjson文字列の例は以下の通り
+```
+{"id": self.s3_count_id,
+    "count": self.s3_shot_count_dic, "total_shots": self.total_shots_dic[self.target_name[index_of_status_type]],
+    "hardware": device_provider,
+    "qpu": device_name, "status": target_status,
+    'date': str(year)+'-'+str(month)+'-'+str(day)
+    }
+```
+
+- delete_quantumTask(quantumTaskArn_name)
+QUEUED状態の指定したタスクをキャンセルできる.
+## lambda_fucntion.py
+
+## lambda_function_money.py
+### shot.py
 実行すると簡易なタスクを投げることができる。確認用。
 
 ### tutorial.ipynb
--- src/AmazonBraketlib.py
+++ src/AmazonBraketlib.py
@@ -5,16 +5,23 @@ from datetime import datetime, date, timedelta
 
 class AmazonBraketlib:
 
-    def __init__(self):
+    def __init__(self, region="us-east-1", clientToken=''):
         """Initialize configuration of braket client and some data
+        Args:
+            region: The AWS Region
+            clientToken: client Token
         """
+        # store bucket name
         self.s3_bucket_name_list = []
+        # store folder name
         self.s3_bucket_folder_name_list = defaultdict(list)
-        self.time_me = 0
         self.total_shots_dic = {}
         self.s3_shot_count_dic = {}
         self.s3_count_id = {}
-        self.braket = boto3.client('braket')
+        self.target_name = ['QUEUED', 'COMPLETED', 'CANCELLED', 'RUNNING']
+        self.region = region
+        self.clientToken = clientToken
+        self.braket = boto3.client('braket', region_name=self.region)
 
     def message_maker(self, date, time, device, count):
         """Make a warning message
@@ -55,9 +62,8 @@ class AmazonBraketlib:
 
         for task in response['quantumTasks']:
             # Returns False if the data is not the given date
-            if task['status'] == target_name[index_of_status_type] and task['createdAt'].date() == date(year, month, day):
-                self.total_shots_dic[target_name[index_of_status_type]] += task['shots']
-                self.time_me = task['createdAt'].time()
+            if task['status'] == self.target_name[index_of_status_type] and task['createdAt'].date() == date(year, month, day):
+                self.total_shots_dic[self.target_name[index_of_status_type]] += task['shots']
 
                 # output s3 information is like this
                 # 'count': {'amazon-braket-issa': 8100,
@@ -86,31 +92,30 @@ class AmazonBraketlib:
                 return False
 
     def get_info(self, year, month, day, device_type, device_provider, device_name, index_of_status_type):
-        """Get information on tasks for a specified device on a specified date
+        """Get task information for a specific device on a specific date
 
         Args:
-            year (_type_): _description_
-            month (_type_): _description_
-            day (_type_): _description_
-            device_type = 'qpu'
-            device_type (str): qpu or quantum-simulator
+            year (int): year
+            month (int): month
+            day (int): day
+            device_type (str): device type (ex. 'qpu', 'qpu-simulator')
             device_provider (str): provider name (ex. amazon, d-wave, rigetti)
             device_name (str): device name (ex. Aspen-10, tn1)
             index_of_status_type (int):  index of status type(0:QUEUED,1:COMPLETED, 2:CANCELLED, 3:RUNNING)
 
         Returns:
+        Task information in json-format string.
+
         """
         self.s3_bucket_name_list = []
         self.s3_bucket_folder_name_list = defaultdict(list)
         self.s3_shot_count_dic = {}
         self.s3_count_id = {}
         self.total_shots_dic = {}
-        # statusのディクショナリーを設定
-        target_name = ['QUEUED', 'COMPLETED', 'CANCELLED', 'RUNNING']
         # 調べるstatusを指定
-        target_status = target_name[index_of_status_type]
+        target_status = self.target_name[index_of_status_type]
         # total_hosts_dicの初期化
-        for I in target_name:
+        for I in self.target_name:
             self.total_shots_dic[I] = 0
         # 指定した日付とタスクの日付の差の上限
         delta = timedelta(seconds=60)
@@ -131,8 +136,6 @@ class AmazonBraketlib:
             filters=own_filters,
             maxResults=100
         )
-        # taskの時刻
-        time_me = 0
         # 1回目のnext_tokenはnullにしておく. すると先頭からsearchしてくれる.
         next_token = ''
         # taskが存在するかのフラグ
@@ -155,11 +158,11 @@ class AmazonBraketlib:
                     next_token = response['nextToken']
 
         return {"id": self.s3_count_id,
-                "count": self.s3_shot_count_dic, "total_shots": self.total_shots_dic[target_name[index_of_status_type]],
+                "count": self.s3_shot_count_dic, "total_shots": self.total_shots_dic[self.target_name[index_of_status_type]],
                 "hardware": device_provider,
                 "qpu": device_name, "status": target_status,
-                'date': str(year)+'-'+str(month)+'-'+str(day),
-                'time': str(time_me)}
+                'date': str(year)+'-'+str(month)+'-'+str(day)
+                }
 
     def delete_quantumTask(self, quantumTaskArn_name):
         """delete specific quantumTask
